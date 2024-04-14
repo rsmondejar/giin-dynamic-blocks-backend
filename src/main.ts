fef33ec2d4fee38bd785874d2e2c20b68a63c5a0
Raúl Sánchez Mondéjar <rsanchez@refineriaweb.com>
2024-04-14 19:22:50 +0200

    Add prisma and swagger libraries

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('GIIN Dynamic Blocks API')
+    .setDescription('API Dynamic Blocks description')
+    .setVersion('0.1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // TODO: Mirar de reducirlo a solo x dominio para el frontend y que use un paramto del fichero .env
+  app.enableCors();
+
+  await app.listen(process.env.PORT || 4000);
 }
+
 bootstrap();
