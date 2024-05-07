94e3effedd1a7cfea6ed7e8a58c707446b6c60e7
Raúl Sánchez Mondéjar <rsanchez@refineriaweb.com>
2024-05-07 19:57:27 +0200

    ADD resources forms skeleton

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { FormsModule } from './forms/forms.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, FormsModule],
 })
 export class AppModule {}
