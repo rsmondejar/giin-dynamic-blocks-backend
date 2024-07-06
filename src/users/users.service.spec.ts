056b0894c5fcf0c6750157c31067cfe01b4e37ab
Raúl Sánchez Mondéjar <rsanchez@refineriaweb.com>
2024-07-06 21:42:36 +0200

    TEST users service

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -3,6 +3,10 @@ import { UsersService } from './users.service';
 import { PrismaService } from '../../prisma/prisma.service';
 import { UserBasicInfo } from './interfaces/user-basic-info.interface';
 import { ConfigModule } from '@nestjs/config';
+import { ObjectId } from 'bson';
+import { HttpException } from '@nestjs/common';
+import { JwtPayload } from '../auth/interfaces/jwt-payload.interface';
+import { UpdatePasswordUserDto } from './dto/update-password-user.dto';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -16,26 +20,156 @@ describe('UsersService', () => {
     service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('findOne', () => {
+    it('should return User not found', async () => {
+      const fakeUserId: string = new ObjectId().toString();
+      await expect(service.findOne(fakeUserId)).rejects.toThrow(HttpException);
+    });
+
+    it('should return a user', async () => {
+      const randomNameSuffix = (Math.random() + 1).toString(36).slice(2, 6);
+      const newUserInfo = {
+        email: `email.${randomNameSuffix}@test.com`,
+        name: `Name ${randomNameSuffix}`,
+        lastName: `Lastname ${randomNameSuffix}`,
+      };
+      const newUser: UserBasicInfo = await service.create({ ...newUserInfo, password: 'password1234' });
+
+      expect(await service.findOne(newUser.id)).toEqual(
+        expect.objectContaining(newUserInfo),
+      );
+
+      await service.remove({ id: newUser.id, authId: newUser.id });
+    });
   });
 
-  describe('findOne', () => {
-    // it('should return User not found', async () => {
-    //   const user = await service.findOne('user-not-found');
-    //   expect(user).toBeDefined();
-    // });
+  describe('findByLogin', () => {
+    it('should return User not found', async () => {
+      const fakeUserId: string = new ObjectId().toString();
+      await expect(service.findOne(fakeUserId)).rejects.toThrow(HttpException);
+    });
 
-    // it('should return a user', async () => {
-    //   // Create user to test
-    //   const newUser: UserBasicInfo = await service.create({
-    //     email: 'email.1@test.com',
-    //     name: 'Name Test',
-    //     lastName: 'Lastname Test',
-    //     password: 'password',
-    //   });
-    //   const user = await service.findOne(newUser.id);
-    //   expect(user).toBeDefined();
-    // });
+    it('should return Invalid Credentials', async () => {
+      const randomNameSuffix = (Math.random() + 1).toString(36).slice(2, 6);
+      const newUserInfo = {
+        email: `email.${randomNameSuffix}@test.com`,
+        name: `Name ${randomNameSuffix}`,
+        lastName: `Lastname ${randomNameSuffix}`,
+      };
+      const newUser: UserBasicInfo = await service.create({ ...newUserInfo, password: 'password1234' });
+
+      await expect(service.findByLogin({ email: newUser.email, password: 'error' }))
+        .rejects.toThrow(HttpException);
+
+      await service.remove({ id: newUser.id, authId: newUser.id });
+    });
+
+    it('should return a user', async () => {
+      const randomNameSuffix = (Math.random() + 1).toString(36).slice(2, 6);
+      const newUserInfo = {
+        email: `email.${randomNameSuffix}@test.com`,
+        name: `Name ${randomNameSuffix}`,
+        lastName: `Lastname ${randomNameSuffix}`,
+      };
+      const password: string = 'password1234';
+      const newUser: UserBasicInfo = await service.create({ ...newUserInfo, password: password });
+
+      expect(await service.findByLogin({ email: newUser.email, password: password })).toEqual(
+        expect.objectContaining(newUserInfo),
+      );
+
+      await service.remove({ id: newUser.id, authId: newUser.id });
+    });
+  });
+
+  describe('findByEmail', () => {
+    it('should return a user', async () => {
+      const randomNameSuffix = (Math.random() + 1).toString(36).slice(2, 6);
+      const newUserInfo = {
+        email: `email.${randomNameSuffix}@test.com`,
+        name: `Name ${randomNameSuffix}`,
+        lastName: `Lastname ${randomNameSuffix}`,
+      };
+      const newUser: UserBasicInfo = await service.create({ ...newUserInfo, password: 'password1234' });
+
+      expect(await service.findByEmail(newUser.email)).toEqual(
+        expect.objectContaining(newUserInfo),
+      );
+
+      await service.remove({ id: newUser.id, authId: newUser.id });
+    });
+  });
+
+  describe('findByPayload', () => {
+    it('should return a user', async () => {
+      const randomNameSuffix = (Math.random() + 1).toString(36).slice(2, 6);
+      const newUserInfo = {
+        email: `email.${randomNameSuffix}@test.com`,
+        name: `Name ${randomNameSuffix}`,
+        lastName: `Lastname ${randomNameSuffix}`,
+      };
+      const newUser: UserBasicInfo = await service.create({ ...newUserInfo, password: 'password1234' });
+
+      const jwtPayload: JwtPayload = { email: newUser.email };
+
+      expect(await service.findByPayload({email: jwtPayload.email})).toEqual(
+        expect.objectContaining(newUserInfo),
+      );
+
+      await service.remove({ id: newUser.id, authId: newUser.id });
+    });
+  });
+
+  describe('updatePassword', () => {
+    it('should return User not found', async () => {
+      const fakeUserId: string = new ObjectId().toString();
+      const payload: UpdatePasswordUserDto = {
+        new_password: 'password1234',
+        old_password: 'error',
+      }
+      await expect(service.updatePassword(payload, fakeUserId)).rejects.toThrow(HttpException);
+    });
+
+    it('should return Invalid Credentials', async () => {
+      const randomNameSuffix = (Math.random() + 1).toString(36).slice(2, 6);
+      const newUserInfo = {
+        email: `email.${randomNameSuffix}@test.com`,
+        name: `Name ${randomNameSuffix}`,
+        lastName: `Lastname ${randomNameSuffix}`,
+      };
+      const newUser: UserBasicInfo = await service.create({ ...newUserInfo, password: 'password1234' });
+
+      const payload: UpdatePasswordUserDto = {
+        new_password: 'password1234',
+        old_password: 'error',
+      }
+
+      await expect(service.updatePassword(payload, newUser.id))
+        .rejects.toThrow(HttpException);
+
+      await service.remove({ id: newUser.id, authId: newUser.id });
+    });
+
+    it('should return a user', async () => {
+      const randomNameSuffix = (Math.random() + 1).toString(36).slice(2, 6);
+      const newUserInfo = {
+        email: `email.${randomNameSuffix}@test.com`,
+        name: `Name ${randomNameSuffix}`,
+        lastName: `Lastname ${randomNameSuffix}`,
+      };
+      const password: string = 'password1234';
+      const newUser: UserBasicInfo = await service.create({ ...newUserInfo, password: password });
+
+      const payload: UpdatePasswordUserDto = {
+        new_password: password,
+        old_password: password,
+      }
+
+      expect(await service.updatePassword(payload, newUser.id)).toEqual(
+        expect.objectContaining(newUserInfo),
+      );
+
+      await service.remove({ id: newUser.id, authId: newUser.id });
+    });
   });
 });
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -116,8 +116,6 @@ export class UsersService {
       },
     });
 
-    console.log(authId);
-
     if (user.id !== authId && !authUser.isAdmin) {
       throw new HttpException(
         'You do not have permissions',
@@ -210,12 +208,12 @@ export class UsersService {
       where: { id },
     });
     if (!user) {
-      throw new HttpException('invalid_credentials', HttpStatus.UNAUTHORIZED);
+      throw new HttpException('USER_NOT_FOUND', HttpStatus.NOT_FOUND);
     }
     // compare passwords
     const areEqual = await compare(payload.old_password, user.password);
     if (!areEqual) {
-      throw new HttpException('invalid_credentials', HttpStatus.UNAUTHORIZED);
+      throw new HttpException('INVALID_CREDENTIALS', HttpStatus.UNAUTHORIZED);
     }
     return this.prisma.user.update({
       where: { id },
