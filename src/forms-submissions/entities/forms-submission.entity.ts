4cb4803dbd878a33d07f9a2d1544e3c5581f7bd8
Raúl Sánchez Mondéjar <rsanchez@refineriaweb.com>
2024-07-10 12:36:44 +0200

    SCRUM-71 Test coverage

--- src/forms-submissions/entities/forms-submission.entity.ts
+++
@@ -1,7 +0,0 @@
-import { QuestionAnswerDto } from '../dto/question-answer.dto';
-
-export class FormsSubmission {
-  id?: string | null;
-  formId?: string | null;
-  answers: QuestionAnswerDto[];
-}
---
+++ src/forms/dto/create-form.dto.spec.ts
@@ -0,0 +1,30 @@
+import { ObjectId } from 'bson';
+import { CreateFormDto } from './create-form.dto';
+import slugify from 'slugify';
+import { CreateFormRequestDto } from './create-form-request.dto';
+
+describe('CreateFormDto', () => {
+  it('should return error invalid form id', async () => {
+    const createFormDto: CreateFormRequestDto = {
+      title: 'Test Form',
+      description: 'Test Form Description',
+      questions: [],
+    };
+    const id: string = new ObjectId().toString().slice(0, 8).toLowerCase();
+    const data: CreateFormDto = {
+      ...createFormDto,
+      slug: slugify(`${createFormDto.title} ${id}}`, { lower: true }),
+      isPublished: true,
+    };
+
+    expect(data).toEqual(
+      expect.objectContaining({
+        title: 'Test Form',
+        description: 'Test Form Description',
+        slug: slugify(`${createFormDto.title} ${id}}`, { lower: true }),
+        isPublished: true,
+      }),
+    );
+    // await expect(service.findOne('id-error')).rejects.toThrow(HttpException);
+  });
+});
